616446d275aac2b80a74105769a8bf85e2596d70
Fatima-Y-Sh <75324775+Fatima-Y-Sh@users.noreply.github.com>
2023-05-23 15:48:30 +0300

    fixed role

--- src/components/screens/Admin.tsx
+++ src/components/screens/Admin.tsx
@@ -95,7 +95,7 @@ const Admin: React.FC = () => {
             toast.error("A valid password should be provided.")
         } else {
             try {
-                const registerUser = await UserService.createUser(user!.token, userInfo.email, userInfo.password, userInfo.role);
+                const registerUser = await UserService.createUser(user!.token, userInfo.email, userInfo.password, 'Admin');
                 toast.dismiss();
                 toast.success("Successfully created the user");
                 setUserModal(false)
@@ -160,7 +160,7 @@ const Admin: React.FC = () => {
                                         as="h1"
                                         className="text-2xl font-gilroyBold leading-6 text-gray-900"
                                     >
-                                       Create a User 🛀
+                                       Create a User 
                                     </Dialog.Title>
 
                                     <div className="mt-5">
@@ -208,67 +208,7 @@ const Admin: React.FC = () => {
                                                 </div>
                                             </div>
 
-                                            <div className={"flex flex-row w-full justify-center items-start"}>
-                                                <div className={"flex flex-col"}>
-                                                    <p className="font-gilroy">Role</p>
-
-                                                    <Listbox value={userInfo.role} onChange={(e) => {
-                                                        setUserInfo({
-                                                            ...userInfo,
-                                                            role: e
-                                                        })
-                                                    }}>
-                                                        <div className="relative mt-1">
-                                                            <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-                                                                <span className="block truncate">{userInfo.role}</span>
-                                                                <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                                                                  <ChevronUpDownIcon
-                                                                      className="h-5 w-5 text-gray-400"
-                                                                      aria-hidden="true"
-                                                                  />
-                                                                </span>
-                                                            </Listbox.Button>
-                                                            <Transition
-                                                                as={Fragment}
-                                                                leave="transition ease-in duration-100"
-                                                                leaveFrom="opacity-100"
-                                                                leaveTo="opacity-0"
-                                                            >
-                                                                <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                                                                    {Object.keys(Role).filter((v) => isNaN(Number(v))).map((role, roleIdx) => (
-                                                                        <Listbox.Option
-                                                                            key={roleIdx}
-                                                                            className={({ active }) =>
-                                                                                `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                                                                                    active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
-                                                                                }`
-                                                                            }
-                                                                            value={role}
-                                                                        >
-                                                                            {({ selected }) => (
-                                                                                <>
-                                                                                  <span
-                                                                                      className={`block truncate ${
-                                                                                          selected ? 'font-medium' : 'font-normal'
-                                                                                      }`}
-                                                                                  >
-                                                                                    {role}
-                                                                                  </span>
-                                                                                    {selected ? (
-                                                                                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
-                                                                                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                                                                                        </span>
-                                                                                    ) : null}
-                                                                                </>
-                                                                            )}
-                                                                        </Listbox.Option>
-                                                                    ))}
-                                                                </Listbox.Options>
-                                                            </Transition>
-                                                        </div>
-                                                    </Listbox>
-                                                </div>
-                                            </div>
+                                        
                                         </div>
 
 
